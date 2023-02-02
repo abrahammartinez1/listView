d55ec6d6bc1e326cf82253010b0f8c8b34e31157
abranllis <47560487+abranllis@users.noreply.github.com>
2023-02-02 09:26:16 +0100

    2

--- src/listView.java
+++ src/listView.java
@@ -1,2 +1,44 @@
-public class listView {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class listView extends JFrame {
+    private List<String> items = new ArrayList<>();
+    private DefaultListModel<String> model = new DefaultListModel<>();
+    private JList<String> list = new JList<>(model);
+
+    public listView() {
+        // Agregar algunos elementos a la lista
+        items.add("Item 1");
+        items.add("Item 2");
+        items.add("Item 3");
+
+        // Actualizar la lista con los elementos iniciales
+        updateList();
+
+        // Añadir la lista al panel de contenido
+        add(new JScrollPane(list), BorderLayout.CENTER);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void updateList() {
+        model.clear();
+        for (String item : items) {
+            model.addElement(item);
+        }
+    }
+
+    public void addItem(String item) {
+        items.add(item);
+        updateList();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new listView());
+    }
 }
